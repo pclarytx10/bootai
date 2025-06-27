ddd482a7d3664bd866375d1fbef3d61fb225abb1
Patrick Clary <patrickm@theclarys.org>
2025-06-26 21:31:02 -0500

    Validate API Key and LLM Query

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    # Check for --verbose flag
+    verbose = False
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, user_prompt, verbose)
+
+
+def generate_content(client, messages, user_prompt, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print()
+        
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
