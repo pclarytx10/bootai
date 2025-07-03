f49f3613bda8dd08071ad488a39a3a7adc2b28c6
Patrick Clary <patrickm@theclarys.org>
2025-07-02 22:19:15 -0500

    Create Run_python function

--- tests.py
+++ tests.py
@@ -1,18 +1,25 @@
-from functions.write_file import write_file
+import os
+
+from functions.run_python import run_python_file
 
 def test():
-    print('Test 1: write_file("calculator", "lorem.txt", "wait, this is not lorem ipsum")')
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Test 1: run_python_file('calculator', 'main.py')")
+    result = run_python_file("calculator", "main.py")
     print(result)
     print()
 
-    print('Test 2: write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")')
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Test 2: run_python_file('calculator', 'tests.py')")
+    result = run_python_file("calculator", "tests.py")
     print(result)
     print()
 
-    print('Test 3: get_file_content("calculator", "/bin/cat") (should error)')
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Test 3: run_python_file('calculator', '../main.py') (should error)")
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+    print()
+
+    print("Test 4: run_python_file('calculator', 'nonexistent.py') (should error)")
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
     print()
 
